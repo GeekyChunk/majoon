d047c377012703dc07cc930b7f4ead9a4795f615
GeekyChunk <ermia1303@gmail.com>
2023-05-16 16:36:43 +0330

    dependencies

--- majoon/settings/production.py
+++ majoon/settings/production.py
@@ -1,3 +1,20 @@
 from .base import *
+import os
 
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': str(os.getenv("DB_NAME")),
+        'USER': str(os.getenv("DB_USER")),
+        'PASSWORD': str(os.getenv("DB_PASS")),
+        'HOST': str(os.getenv("DB_HOST")),
+        'PORT': str(os.getenv("DB_PORT")),
+    }
+}
+
+
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = str(os.getenv("STATIC_URL"))
+
